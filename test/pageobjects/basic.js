281e17388b035e14afddd92a5a27f95ab321b6d1
PrihodkoMax <1986maxim1508@gmail.com>
2022-12-29 14:13:19 +0200

    Start using WebdriverIO

---
+++ test/pageobjects/basic.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('webdriver.io page', () => {
+	it('should have the right title', () => {
+		browser.url('https://webdriver.io');
+		const title = browser.getTitle();
+		assert.stringEqual(title, 'WebdriverIO - Next-gen webdriver test framework for Node.js');
+	});
+});
