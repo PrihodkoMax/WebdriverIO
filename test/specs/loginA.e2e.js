b968e6aec4749118de182e744b004fc6141411ea
Konstantin Maev <sochislowlife@coreteam.dev>
2023-01-06 18:53:47 +0200

    8
    add login to admin account

--- test/specs/loginA.e2e.js
+++ test/specs/loginA.e2e.js
@@ -1,20 +1,19 @@
-import LoginPage from '../pageobjects/login.page.js';
-import MainPage from '../pageobjects/main.page.js';
-
-describe("My Login application", () => {
+import LoginPage from '../pageobjects/loginA.page.js';
+import MainPage from '../pageobjects/mainA.page.js';
+describe("My LoginA application", () => {
 	it("Should login with valid credentials", async () => {
 
 		// Открываем страницу авторизации
 		await LoginPage.open();
 
 		// Логинемся
-		await LoginPage.login('Maksym QA', '1Test#01');
+		await LoginPage.login('admin.maev', 'Q!q12345678');
 
 		// Проверяем наличие заголовка
 		await expect(MainPage.menuItemMyAccounts).toBeExisting();
 
 		// Сверяем текст заголовка
-		await expect(MainPage.menuItemMyAccounts).toHaveTextContaining('Мои счета');
+		await expect(MainPage.menuItemMyAccounts).toHaveTextContaining('MONO');
 
 	});
 });
