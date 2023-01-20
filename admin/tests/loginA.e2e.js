3fecc4bce4651bd93467383f4eeef0707d584c8d
PrihodkoMax <1986maxim1508@gmail.com>
2023-01-20 17:42:31 +0200

    Старт проэкта по общему патерну

--- admin/tests/loginA.e2e.js
+++ admin/tests/loginA.e2e.js
@@ -1,13 +1,5 @@
 import LoginPage from '../pages/loginA.page.js';
-<<<<<<< HEAD:admin/tests/loginA.e2e.js
-import MainPage from '../pages/mainA.page.js';
-=======
 import MainPage from '../pages/mainA.page.js.js';
-<<<<<<< HEAD
->>>>>>> 8e7a2b56a70b441fc299ed3542121d44c4ed759f:test/specs/loginA.e2e.js
-=======
->>>>>>> 91616c740514a5da406f9c7ef6f173ffaced5d91:test/specs/loginA.e2e.js
->>>>>>> master
 
 describe("My LoginA application", () => {
 	it("Should login with valid credentials", async () => {
