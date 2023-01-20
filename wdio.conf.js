124afa96c0a0ce82ffa398005931b8baa741c2b5
PrihodkoMax <1986maxim1508@gmail.com>
2023-01-20 16:56:01 +0200

    Переделал структуру проэкта по согласованному общему шаблону

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,7 +23,8 @@ export const config = {
 	// will be called from there.
 	//
 	specs: [
-		'./test/specs/**/*.js'
+		'./admin/tests/**/*.js',
+		'./client/tests/**/*.js'
 
 	],
 	// Patterns to exclude.
